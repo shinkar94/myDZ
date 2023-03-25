03f3d5eee211efc402718e8c4bf36e9c9ac40a49
shinkar94 <romka94shinkarenko@gmail.com>
2023-03-25 15:42:04 +0300

    hw 8 update reducer

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,7 +7,6 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-            console.log(state)
             let sortByName = (a = state[0], b = state[0]) => (
                 action.payload === "up" ? a.name > b.name : a.name < b.name
             ) ? 1 : -1
@@ -15,8 +14,8 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
             return newState
         }
         case 'check': {
-
-            return state.filter(el => el.age >= action.payload)
+            let sortByName = (a = state[0], b = state[0]) => a.name < b.name ? 1 : -1
+            return state.sort(sortByName).filter(el => el.age >= action.payload)
         }
         default:
             return state
